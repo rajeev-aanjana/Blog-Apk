a465ffa635321824020dc83198b3e3db0799dd0e
Rajeev Aanjana <rajeevanjana143@gmail.com>
2024-06-30 00:54:35 +0530

    Header Section COmplete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,20 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
  
 function App() {
   return (
     <BrowserRouter>
+    <Header />
 <Routes>
   <Route path='/' element={<Home />}></Route>
   <Route path='/about' element={<About/>}></Route>
   <Route path='/sign-in' element={<Signin />}></Route>
   <Route path='/sign-up' element={<Signup />}></Route>
   <Route path='/dashboard' element={<Dashboard />}></Route>
-  
   <Route path='/projects' element={<Projects/> }></Route>
+
 </Routes>
     </BrowserRouter>
   );
