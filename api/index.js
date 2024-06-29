ee1d389b7151c51dcd7218203340dd178c4f6740
Rajeev Aanjana <rajeevanjana143@gmail.com>
2024-06-30 04:01:16 +0530

    middleware and a signup is created

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from './routes/user.routes.js'
+import authRoutes from './routes/auth.routes.js'
 
 dotenv.config();
 
@@ -16,8 +17,21 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
 
 app.use("/api/user", userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err,req,res,next)=>{
+  const statuscode = err.statuscode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode.json({
+    success:false,
+    statuscode,
+    message
+  }))
+})
--- api/models/user.models.js
+++ api/models/user.models.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    userName: {
+    username: {
       type: String,
       required: true,
       unique: true,
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-    },
+    }
   },
   { timestamps: true }
 );
