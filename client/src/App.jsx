eb41a35078730f78fedd6797a010112cc2122421
Rajeev Aanjana <rajeevanjana143@gmail.com>
2024-06-30 13:08:52 +0530

    Footer Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,26 @@
-import {BrowserRouter, Routes , Route} from 'react-router-dom'
-import Home from './pages/Home';
-import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import Header from './components/Header';
- 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+
 function App() {
   return (
     <BrowserRouter>
-    <Header />
-<Routes>
-  <Route path='/' element={<Home />}></Route>
-  <Route path='/about' element={<About/>}></Route>
-  <Route path='/sign-in' element={<Signin />}></Route>
-  <Route path='/sign-up' element={<Signup />}></Route>
-  <Route path='/dashboard' element={<Dashboard />}></Route>
-  <Route path='/projects' element={<Projects/> }></Route>
-
-</Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<Signup />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
